70f87c0fa05f4d6e77e3a13ea755583ab3ac8984
Bex Birkett <bex.birkett@outlook.com>
2020-08-04 09:39:57 +0100

    git issue, moved to new repo

---
+++ README.md
@@ -0,0 +1,3 @@
+# Dice Game
+
+This is a git practice game called 'Dice Game'
---
+++ app.js
@@ -0,0 +1,41 @@
+const button = document.getElementById('button');
+const rollResult = document.getElementById('rollResult');
+const score = document.getElementById('score');
+
+const dice = {
+    sides: 20,
+    roll() {
+      return Math.floor(Math.random() * this.sides) + 1;
+    }
+}
+const scoreTrack = {
+    rollResultNum: 0,
+    scoreNum: 0,
+    calc () {
+        this.scoreNum += this.rollResultNum;
+    },
+}
+const playAgain = () => {
+    button.textContent = 'Play Again?';
+    scoreTrack.rollResultNum = 0;
+    scoreTrack.scoreNum = 0;
+}
+
+
+//Prints dice roll & score to the page
+button.addEventListener('click',() => {
+    button.textContent = 'Roll';
+    scoreTrack.rollResultNum = dice.roll();
+    scoreTrack.calc();
+    rollResult.src = `/img/dice${scoreTrack.rollResultNum}.png`;
+    score.textContent = scoreTrack.scoreNum;
+    console.log(scoreTrack.scoreNum)
+    if (scoreTrack.rollResultNum == 1) {
+        score.innerHTML = "You lose!";
+        playAgain();
+    }
+    else if (scoreTrack.scoreNum >= 40){
+        score.innerHTML = `You win! Score: ${scoreTrack.scoreNum}.`;
+        playAgain();
+    }
+})
